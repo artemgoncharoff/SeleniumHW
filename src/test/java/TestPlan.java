be5cc63977ca5f3045a32befcdfa9f3dd02d96b9
Artem <adgoncharov_1@edu.hse.ru>
2020-11-30 16:49:08 +0300

    first commit

---
+++ src/test/java/TestPlan.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class TestPlan {
+    private static final WebDriver driver = new ChromeDriver();
+
+
+}
---
+++ src/test/java/Utils.java
@@ -0,0 +1,10 @@
+public class Utils {
+    final static String BASE_URL = "https://ruswizard.site/test/";
+
+    /*
+        Используйте тот chromedriver, который подходит под вашу систему!
+     */
+    //final static String CHROME_DRIVER_LOCATION = "chromedriver_mac";
+    final static String CHROME_DRIVER_LOCATION = "chromedriver_win";
+    //final static String CHROME_DRIVER_LOCATION = "chromedriver_linux";
+}
